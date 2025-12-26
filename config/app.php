3f706907881017bcc4a754cf39b07172b8d06cef
Billy R Baldwin <billyraybaldwin@gmail.com>
2025-12-26 10:13:19 -0600

    Starting to define proper seperation of dev/live environments.

--- config/app.php
+++ config/app.php
@@ -32,33 +32,4 @@ if (session_status() === PHP_SESSION_NONE) {
  * Wrapped in if(!function_exists) to prevent redeclaration errors
  */
 
-if (!function_exists('sanitizeOutput')) {
-    function sanitizeOutput($data) {
-        return htmlspecialchars($data ?? '', ENT_QUOTES, 'UTF-8');
-    }
-}
 
-if (!function_exists('generateCsrfToken')) {
-    function generateCsrfToken() {
-        startSecureSession();
-        if (!isset($_SESSION[CSRF_TOKEN_NAME])) {
-            $_SESSION[CSRF_TOKEN_NAME] = bin2hex(random_bytes(32));
-        }
-        return $_SESSION[CSRF_TOKEN_NAME];
-    }
-}
-
-/**
- * Verify CSRF token
- * @param string $token
- * @return bool
- */
-if (!function_exists('verifyCsrfToken')) {
-    function verifyCsrfToken($token) {
-        startSecureSession();
-        return isset($_SESSION[CSRF_TOKEN_NAME]) && hash_equals($_SESSION[CSRF_TOKEN_NAME], $token);
-    }
-}
-
-/**
- * Sanitize output to prevent XSS
