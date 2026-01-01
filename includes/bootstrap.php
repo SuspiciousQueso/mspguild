35e45bb34724517761f760c2be916b632f2b318a
Billy R Baldwin <billyraybaldwin@gmail.com>
2025-12-31 18:24:06 -0600

    Cleand up code, will elaborate in release

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -2,9 +2,9 @@
 /**
  * MSPGuild Global Bootstrap
  */
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 // Load the merged configuration
 require_once __DIR__ . '/../config/app.php';
