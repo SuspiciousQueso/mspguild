0d3f8bb876ccfc440e377c96b9a06be5f09b2809
Billy R Baldwin <billyraybaldwin@gmail.com>
2025-12-31 21:57:51 -0600

    Cleand up code, will elaborate in release

--- public/logout.php
+++ public/logout.php
@@ -1,7 +1,9 @@
 <?php
-require_once __DIR__ . '/../includes/Auth.php';
+require_once __DIR__ . '/../includes/bootstrap.php';
+use MSPGuild\Core\Auth;
 
-logoutUser();
+// Require login
+Auth::requireAuth();
 
 header('Location: ' . SITE_URL . '/index.php');
 exit;
