13d703b8a2c8f0c127a5652a240f8102b7a0493a
Billy R Baldwin <billyraybaldwin@gmail.com>
2025-12-31 22:58:57 -0600

    Cleand up code, will elaborate in release

--- includes/header.php
+++ includes/header.php
@@ -36,98 +36,54 @@ $isLoggedIn  = $isLoggedIn  ?? false;
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
+                <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link <?php echo $currentPage === 'index' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/index.php">Home</a>
                     </li>
-
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo defined('FRONTDESK_URL') ? FRONTDESK_URL : (SITE_URL . '/modules/frontdesk/index.php'); ?>">
-                            FrontDesk
+                        <a class="nav-link" href="<?php echo FRONTDESK_URL; ?>">
+                            <i class="bi bi-inbox"></i> FrontDesk
                         </a>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link <?php echo $currentPage === 'contact' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/contact.php">Contact</a>
                     </li>
-
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo defined('RESUME_URL') ? RESUME_URL : '#'; ?>" target="_blank">Resume</a>
+                        <a class="nav-link" href="<?php echo RESUME_URL; ?>" target="_blank">
+                            <i class="bi bi-file-earmark-person"></i> Resume
+                        </a>
                     </li>
-
-                    <?php if (!empty($isLoggedIn)): ?>
+                    <?php if ($isLoggedIn): ?>
                         <li class="nav-item">
-                            <a class="nav-link <?php echo $currentPage === 'dashboard' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/dashboard.php">Dashboard</a>
+                            <a class="nav-link <?php echo $currentPage === 'dashboard' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/dashboard.php">
+                                <i class="bi bi-speedometer2"></i> Dashboard
+                            </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link <?php echo $currentPage === 'profile' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/user_profile_update.php">Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo SITE_URL; ?>/logout.php">Logout</a>
+                            <a class="nav-link" href="<?php echo SITE_URL; ?>/logout.php">
+                                <i class="bi bi-box-arrow-right"></i> Logout
+                            </a>
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
-                            <a class="nav-link <?php echo $currentPage === 'register' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/user_registration.php">Register</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link <?php echo $currentPage === 'login' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/login.php">Login</a>
+                            <a class="nav-link <?php echo $currentPage === 'login' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/login.php">
+                                <i class="bi bi-box-arrow-in-right"></i> Login
+                            </a>
                         </li>
                     <?php endif; ?>
                 </ul>
-
                 <?php else: ?>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-                    <div class="container">
-                        <a class="navbar-brand fw-bold" href="<?php echo SITE_URL; ?>/index.php">
-                            <i class="bi bi-shield-check"></i> <?php echo SITE_NAME; ?>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo $currentPage === 'register' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/user_registration.php">
+                            <i class="bi bi-person-plus"></i> Register
                         </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-
-                                <li class="nav-item">
-                                    <a class="nav-link <?php echo $currentPage === 'index' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/index.php">Home</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo defined('FRONTDESK_URL') ? FRONTDESK_URL : (SITE_URL . '/modules/frontdesk/index.php'); ?>">
-                                        FrontDesk
-                                    </a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link <?php echo $currentPage === 'contact' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/contact.php">Contact</a>
-                                </li>
-
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo defined('RESUME_URL') ? RESUME_URL : '#'; ?>" target="_blank">Resume</a>
-                                </li>
-                                <li class="nav-item">
-                        <a class="nav-link <?php echo $currentPage === 'login' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/login.php"><i class="bi bi-box-arrow-in-right"></i>Login</a>
-                    </li>
-
-                       <!--        <?php // if (!empty($isLoggedIn)): ?>
-                                    <li class="nav-item">
-                                        <a class="nav-link <?php //echo $currentPage === 'dashboard' ? 'active' : ''; ?>" href="<?php //echo SITE_URL; ?>/dashboard.php">Dashboard</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link <?php //echo $currentPage === 'profile' ? 'active' : ''; ?>" href="<?php //echo SITE_URL; ?>/user_profile_update.php">Profile</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="<?php //echo SITE_URL; ?>/logout.php">Logout</a>
-                                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link <?php //echo $currentPage === 'register' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/user_registration.php"><i class="bi bi-person-plus"></i>Register</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php //echo $currentPage === 'login' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/login.php"><i class="bi bi-box-arrow-in-right"></i>Login</a>
+                        <a class="nav-link <?php echo $currentPage === 'login' ? 'active' : ''; ?>" href="<?php echo SITE_URL; ?>/login.php">
+                            <i class="bi bi-box-arrow-in-right"></i> Login
+                        </a>
                     </li>
-                <?php //endif; ?>
-                -->
+                <?php endif; ?>
             </div>
         </div>
     </nav>
-<?php endif; ?>
